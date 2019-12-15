003132189af2c904bf2843e2ba3fba86879421b4
Jake Screen <jscreen@udel.edu>
2019-12-15 02:12:32 -0500

    Updates

---
+++ precache-manifest.1487d8f82d632dbc01b65bcbd456d305.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d3482d96845a18f51c2143e81225898",
+    "url": "/webappfinal/index.html"
+  },
+  {
+    "revision": "02322d14a7f593771dde",
+    "url": "/webappfinal/static/css/main.c0d4cc9d.chunk.css"
+  },
+  {
+    "revision": "9045013b70d35c743b39",
+    "url": "/webappfinal/static/js/2.67e2c742.chunk.js"
+  },
+  {
+    "revision": "02322d14a7f593771dde",
+    "url": "/webappfinal/static/js/main.1368e2e6.chunk.js"
+  },
+  {
+    "revision": "bac2d157c8b72af8156f",
+    "url": "/webappfinal/static/js/runtime-main.eb2ac904.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/webappfinal/static/media/logo.25bf045c.svg"
+  }
+]);
